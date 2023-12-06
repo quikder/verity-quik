fc7ba163a350c1d7da7942689e070012dc1f1727
Fernando Lopez <fernandojalope343@icloud.com>
2023-12-06 14:41:30 -0500

    multipicker

---
+++ src/components/MultiPicker/index.tsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import { Controller } from "react-hook-form";
+import DropDownPicker from "react-native-dropdown-picker";
+import { HelperText } from "react-native-paper";
+import { useTheme } from "styled-components/native";
+
+export const MultiPicker: React.FC<Props> = ({
+	items,
+	name,
+	placeholder,
+	control,
+	rules,
+	schema = { label: "label", value: "value" },
+}) => {
+	const [open, setOpen] = useState(false);
+	const [defaultValue, setDefaultValue] = useState([]);
+	const theme = useTheme();
+
+	return (
+		<>
+			<Controller
+				name={name}
+				control={control}
+				rules={rules}
+				render={({
+					field: { onChange },
+					fieldState: { error: errorForm },
+				}) => {
+					return (
+						<>
+							<DropDownPicker
+								listMode="SCROLLVIEW"
+								schema={schema}
+								multiple={true}
+								mode="BADGE"
+								showBadgeDot={false}
+								placeholder={placeholder}
+								open={open}
+								value={defaultValue}
+								items={items}
+								setOpen={setOpen}
+								setValue={setDefaultValue}
+								onChangeValue={onChange}
+								style={{
+									borderWidth: 0,
+									borderBottomWidth: errorForm ? 2 : 0.3,
+									borderRadius: 0,
+									borderBottomColor: errorForm
+										? theme.colors.error
+										: theme.colors.onSurfaceVariant,
+									paddingLeft: 16,
+								}}
+								textStyle={{
+									letterSpacing: 0.15,
+									fontSize: 16,
+									textTransform: "capitalize",
+								}}
+								placeholderStyle={{
+									color: errorForm
+										? theme.colors.error
+										: theme.colors.onSurfaceVariant,
+								}}
+								dropDownContainerStyle={{
+									borderColor: theme.colors.onSurfaceVariant,
+									borderWidth: 0.3,
+								}}
+								badgeStyle={{
+									borderRadius: 20,
+								}}
+								badgeColors={theme.colors.primary}
+								badgeTextStyle={{ color: "#fff" }}
+							/>
+
+							{errorForm && (
+								<HelperText
+									type={errorForm ? "error" : "info"}
+									style={{ marginTop: -7, marginBottom: -7 }}
+								>
+									{errorForm.message}
+								</HelperText>
+							)}
+						</>
+					);
+				}}
+			/>
+		</>
+	);
+};
+
+interface Props {
+	items: ItemType[];
+	placeholder: string;
+	name: string;
+	control: any;
+	rules?: object;
+	schema?: ItemType;
+}
+
+interface ItemType {
+	label: string;
+	value: string;
+}
